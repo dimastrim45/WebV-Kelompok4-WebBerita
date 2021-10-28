ff4ad6e0ea15a26fa1cc7ab0c7acdaedf13ca97a
dimastrim45 <dimastrim45@github.com>
2021-10-28 16:22:06 +0700

    Add Project using laravel 8

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/health', function(){
+    return view('health', [
+        "title" => "Health"
+    ]);
+});
+
+Route::get('/education', function(){
+    return view('education', [
+        "title" => "Education"
+    ]);
+});
+
+Route::get('/techno', function(){
+    return view('techno', [
+        "title" => "Technology"
+    ]);
+});
+
+Route::get('/food', function(){
+    return view('food', [
+        "title" => "Food"
+    ]);
+});
+
+Route::get('/about', function(){
+    return view('about', [
+        "title" => "About",
+        "name" => "Sam Martin",
+        "email" => "sammartintm45@gmail.com",
+        "image" => "profile.jpg"
+    ]);
+});
