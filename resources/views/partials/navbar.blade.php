fca86e7d5dc61db7b408ca26d8046b142e5a2eda
risky-ade <riskyadesucahyo@gmail.com>
2022-01-12 21:30:25 +0700

    fix css

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,31 +1,12 @@
 <link rel="stylesheet" href="css/style.css">
 <nav class="navbar mb-5 navbar-expand-lg navbar-light bg-light shadow-sm fixed-top">
     <div class="container">
-        <a class="navbar-brand " href="/">WEBNEWS</a>
+        <a class="navbar-brand pb-0" href="/"><img src="{{ $url=asset('img/logoberita.png') }}" alt="webnews" width="70">WebNEWS</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        {{-- <div class="collapse navbar-collapse" id="navbarNav">
-            <div class="sosial">
-                        <ul class="nav">
-                            <span style=" font-size: 12px; font-weight: bold;">Find Us on : </span>
-                            <li class="nav-icon">
-                                <a href="http://facebook.com"><img src="img/imagesite/icon/facebook.png"></a>
-                            </li>
-                            <li class="nav-icon">
-                                <a href="http://twitter.com"><img src="img/imagesite/icon/twitter.png"></a>
-                            </li>
-                            <li class="nav-icon">
-                                <a href="http://linkedin.com"><img src="img/imagesite/icon/linkedin.png"></a>
-                            </li>
-                            <li class="nav-icon">
-                                <a href="http://youtube.com"><img src="img/imagesite/icon/youtube.png"></a>
-                            </li>
-                        </ul>
-            </div>
-        </div> --}}
         <form class="d-flex pull-right">
             <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success" type="submit">Search</button>
